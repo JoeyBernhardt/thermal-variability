364b7c78130d53efddae0522a11590430c31da9d
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-12-31 10:13:53 -0800

    trying something new to find limits on tmin and tmaxes :tada:

---
+++ Tetraselmis_experiment/R/27_limits_bootstrapped_curves.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+fits_variable <- read_csv("Tetraselmis_experiment/data-processed/boot_fits_resample_v.csv")
+
+### goal: find the tmin and tmaxes of the bootstrapped curves
+
+breadth_function <- function(data) {
+  all <- fits_variable[1,]
+  x <- seq(-10, 45, by = 0.1)
+  tpc<-function(x){
+    res<-all$a.list[1]*exp(all$b.list[1]*x)*(1-((x-all$z.list[1])/(all$w.list[1]/2))^2)
+    res
+  }
+  ## step 2 
+  variable_predictions <- function(x) {
+    y <- tpc(x)
+  }
+  
+  predicted_growth_variable <- sapply(x, variable_predictions)
+  predicted_growth_variable2 <- data.frame(x, predicted_growth_variable) %>% 
+    rename(temperature = x, 
+           growth.rate = predicted_growth_variable) %>% 
+    filter(growth.rate > 0) %>% 
+    top_n(n = -20, wt = growth.rate) %>% 
+    summarise_each(funs(min, max), temperature)
+  # data.frame(all$isolate.code, predicted_growth_variable2)
+}
+
+
+all_split <- fits_variable %>% 
+  split(.$curve.id.list)
+
+results_breadth <- all_split %>% 
+  map_df(breadth_function, .id = "curve_id")
+
