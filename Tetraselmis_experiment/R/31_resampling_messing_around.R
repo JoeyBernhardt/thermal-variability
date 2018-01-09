81ea5b06bcc3a86df397ec3b9db84c9e33749220
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-01-09 11:26:37 -0800

    adding summaries from resampling

--- Tetraselmis_experiment/R/31_resampling_messing_around.R
+++ Tetraselmis_experiment/R/31_resampling_messing_around.R
@@ -2,7 +2,9 @@
 
 
 library(tidyverse)
-
+library(colormap)
+library(cowplot)
+library(rootSolve)
 
 
 d <- read_csv("Tetraselmis_experiment/data-processed/boot_fits_resample.csv")
@@ -55,4 +57,55 @@ boot_limits_10 <- all_predictions10 %>%
             q97.5=quantile(predictions, probs=0.975),
             mean = mean(predictions)) 
 
+ic <- colormap(colormap = colormaps$viridis, nshades = 8, format = "hex",
+               alpha = 1, reverse = FALSE)
 
+p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x)) 
+p + 
+  geom_vline(xintercept = 24.5681, color = ic[3], alpha = 0.5) +
+  geom_hline(yintercept = 0, color = "grey") +
+  # geom_ribbon(aes(x = x, ymin = q2.5, ymax = q97.5, linetype=NA), data = boot_limits, fill = ic[3], alpha = 1) +
+  geom_ribbon(aes(x = x, ymin = q2.5, ymax = q97.5, linetype=NA), data = boot_limits_10, fill = ic[4], alpha = 0.5) +
+  coord_cartesian(ylim = c(-0.2, 1.7), xlim = c(0, 32))
+  
+
+
+fits10 <- d10 %>% 
+  rename(z = z.list,
+         a = a.list, 
+         b = b.list, 
+         w = w.list)
+
+
+cf10 <- fits10 %>%
+  group_by(curve.id.list) %>% 
+  mutate(tmax = ifelse(length(uniroot.all(function(x) nbcurve(x, z, w, a, b),c(topt.list,150)))==0, NA,
+                       uniroot.all(function(x) nbcurve(x, z, w,  a, b),c(topt.list,150)))) %>% 
+  mutate(tmin = ifelse(length(uniroot.all(function(x) nbcurve(x, z,w, a, b),c(-1.8,topt.list)))==0, NA,
+                       uniroot.all(function(x) nbcurve(x, z, w, a, b),c(-1.8,topt.list))))
+
+
+fits_10 <- cf10 %>% 
+  filter(!is.na(tmax)) %>% 
+  mutate(tmin = ifelse(is.na(tmin), -1.8, tmin)) %>% 
+  ungroup()
+
+
+fits_above_freezing_constant_10 <- fits_real %>% 
+  mutate(breadth = tmax - tmin) %>%
+  summarise(w_low=quantile(w, probs=0.025),
+            w_high=quantile(w, probs=0.975),
+            w_mean=mean(w),
+            tmin_low=quantile(tmin, probs=0.025),
+            tmin_high=quantile(tmin, probs=0.975),
+            tmin_mean=mean(tmin),
+            tmax_low=quantile(tmax, probs=0.025),
+            tmax_high=quantile(tmax, probs=0.975),
+            tmax_mean=mean(tmax),
+            breadth_low=quantile(breadth, probs=0.025),
+            breadth_high=quantile(breadth, probs=0.975),
+            breadth_mean=mean(breadth),
+            topt_low=quantile(topt.list, probs=0.025),
+            topt_high=quantile(topt.list, probs=0.975),
+            topt_mean=mean(topt.list)) 
+write_csv(fits_above_freezing_constant_10, "Tetraselmis_experiment/data-processed/fits_above_freezing_constant_10.csv")
