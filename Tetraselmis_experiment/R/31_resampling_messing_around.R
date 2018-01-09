b6809a0b55afe20e4e0d7062d4be8c6fac112ca1
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-01-09 10:57:01 -0800

    a bit more messing around

---
+++ Tetraselmis_experiment/R/31_resampling_messing_around.R
@@ -0,0 +1,31 @@
+### limiting resampling exploring
+
+
+library(tidyverse)
+
+
+
+d <- read_csv("Tetraselmis_experiment/data-processed/boot_fits_resample.csv")
+
+
+fits_split <- d %>% 
+  sample_n(size = 500, replace = FALSE) %>% 
+  filter(rsqr.list > 0.98) %>% 
+  split(.$curve.id.list)
+
+prediction_function <- function(curve1) {
+  x <- seq(-3, 38, 0.1)
+  predictions <- curve1$a.list[[1]]*exp(curve1$b.list[[1]]*x)*(1-((x-curve1$z.list[[1]])/(curve1$w.list[[1]]/2))^2)
+  data.frame(x, predictions)
+}
+
+## make predictions for each set of parameter estimates. 
+all_predictions <- fits_split %>% 
+  map_df(prediction_function) 
+
+## get the upper and lower limits of the predicted growth rates at each value of x (temperature)
+boot_limits_variable_limited <- all_predictions %>% 
+  group_by(x) %>% 
+  summarise(q2.5=quantile(predictions, probs=0.025),
+            q97.5=quantile(predictions, probs=0.975),
+            mean = mean(predictions)) 
