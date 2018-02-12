97c86974c5211076f58a12acff0df60479fee060
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-02-12 11:48:20 -0800

    time series growth curve fitting

--- Tetraselmis_experiment/R/37_fit_growth_temp.R
+++ Tetraselmis_experiment/R/37_fit_growth_temp.R
@@ -39,152 +39,6 @@ nbcurve<-function(temp,z,w,a,b, hours){
 }
 
 
-
-avals<-seq(-0.2,1.2,0.1)		
-bvals<-seq(-0.2,0.3,0.05)
-mod.list<-list()
-AIC.list<-c()
-
-for(ia in 1:length(avals)){
-  for(ib in 1:length(bvals)){
-    a.guess<-avals[ia]
-    b.guess<-bvals[ib]
-    res2<-try(fit<-mle2(dat$cell_density~dnorm(mean=nbcurve(dat$temperature,z=z,w=w,a=a,b=b,dat$hours),sd=s),start=list(z=16,w=30,a=0.2,b=0.1,s=0.3),
-                        skip.hessian=TRUE,data=dat))
-    
-    
-    if(class(res2)!="try-error"){
-      mod.list<-append(mod.list,fit)
-      AIC.list<-append(AIC.list,AIC(fit))
-    }
-  }
-}
-
-res2<-try(fit<-mle2(dat$cell_density~dnorm(mean=nbcurve(dat$temperature,z=z,w=w,a=a,b=b, hours = hours),sd=s),start=list(z=16,w=30,a=0.2,b=0.1,s=0.3),
-                    skip.hessian=TRUE,data=dat))
-
-
-
-# Create new vector of unique curve.id values
-curve.id.list<-unique(dat.full$curve.id)	
-
-# Create empty vectors to populate with parameter values and trait estimates
-z.list<-rep(NA, length(curve.id.list))				#Parameter 'z'
-w.list<-rep(NA, length(curve.id.list))				#Parameter 'w', which is the temperature niche width
-a.list<-rep(NA, length(curve.id.list))				#Parameter 'a'
-b.list<-rep(NA, length(curve.id.list))				#Parameter 'b'
-topt.list<-rep(NA, length(curve.id.list))			#Topt, Optimum temperature for growth
-maxgrowth.list<-rep(NA, length(curve.id.list))		#Maximum growth rate (i.e. growth rate at Topt)
-rsqr.list<-rep(NA, length(curve.id.list))			#R^2 values for all fits
-s.list<-rep(NA, length(curve.id.list))				#Error
-n.list<-rep(NA, length(curve.id.list))				#Number of growth rate measurements used in the curve
-
-### Loop through all curve.id.list values to estimate parameters for all curves
-### on my computer this takes QUITE a while! This code is taken directly from
-### Mridul Thomas, http://mridulkthomas.weebly.com/data--code.html
-
-## to skip this step, just read in the outputs of this step:
-## ("Tetraselmis_experiment/data-processed/boot_fits.csv")
-
-
-for(i in 1:length(curve.id.list)){
-  print(i)
-  
-  # Take a subset of the data corressponding to the ith curve.id.list value
-  dat<-subset(dat.full,dat.full$curve.id==curve.id.list[i])
-  
-  # guess starting values for parameters 'z' and 'w'
-  z.guess<-mean(dat$temperature[dat$growth.rate==max(dat$growth.rate)])		#starting estimates for 'z'
-  w.guess<-diff(range(dat$temperature))										#starting estimates for niche width
-  
-  ## This loop fits the model using a range of different starting guesses. We choose the best one using AIC. This helps find good solutions even if there are
-  # convergence problems.
-  # Starting estimates for parameters 'a' and 'b' use a plausible range but with broadly spaced estimates to speed up fitting. 
-  avals<-seq(-0.2,1.2,0.1)		
-  bvals<-seq(-0.2,0.3,0.05)
-  mod.list<-list()
-  AIC.list<-c()
-  
-  for(ia in 1:length(avals)){
-    for(ib in 1:length(bvals)){
-      a.guess<-avals[ia]
-      b.guess<-bvals[ib]
-      res2<-try(fit<-mle2(dat$growth.rate~dnorm(mean=nbcurve(dat$temperature,z=z,w=w,a=a,b=b),sd=s),start=list(z=z.guess,w=w.guess,a=a.guess,b=b.guess,s=0.3),
-                          skip.hessian=TRUE,data=dat))
-      if(class(res2)!="try-error"){
-        mod.list<-append(mod.list,fit)
-        AIC.list<-append(AIC.list,AIC(fit))
-      }
-    }
-  }
-  
-  # Identify the best model from the list and save coefficients and R^2 values
-  if(!is.null(AIC.list)){
-    bestmodind<-which(AIC.list==min(AIC.list))
-    if(length(bestmodind)>1){
-      bestmodind<-sample(bestmodind,1)
-    }
-    bestmod<-mod.list[[bestmodind]]
-    cfs<-coef(bestmod)
-    expected<-nbcurve(dat$temperature,cfs[[1]],cfs[[2]],cfs[[3]],cfs[[4]])
-    rsqr<-1-sum((dat$growth.rate-expected)^2)/sum((dat$growth.rate-mean(dat$growth.rate))^2)
-  }
-  
-  # If the quick fit yielded poor results (low R^2), try a more thorough search through parameter space
-  if(rsqr<0.95){
-    avals<-seq(-0.2,1.2,0.02)
-    bvals<-seq(-0.2,0.3,0.02)
-    mod.list<-list()
-    AIC.list<-c()
-    for(ia in 1:length(avals)){
-      for(ib in 1:length(bvals)){
-        a.guess<-avals[ia]
-        b.guess<-bvals[ib]
-        res2<-try(fit<-mle2(dat$growth.rate~dnorm(mean=nbcurve(dat$temperature,z=z,w=w,a=a,b=b),sd=s),start=list(z=z.guess,w=w.guess,a=a.guess,b=b.guess,s=0.3),
-                            skip.hessian=TRUE,data=dat))
-        if(class(res2)!="try-error"){
-          mod.list<-append(mod.list,fit)
-          AIC.list<-append(AIC.list,AIC(fit))
-        }
-      }
-    }
-    # Identify the best model from the list and save coefficients and R^2 values
-    bestmodind<-which(AIC.list==min(AIC.list))
-    if(length(bestmodind)>1){
-      bestmodind<-sample(bestmodind,1)
-    }
-    
-    bestmod<-mod.list[[bestmodind]]
-    cfs<-coef(bestmod)
-    expected<-nbcurve(dat$temperature,cfs[[1]],cfs[[2]],cfs[[3]],cfs[[4]])
-    rsqr<-1-sum((dat$growth.rate-expected)^2)/sum((dat$growth.rate-mean(dat$growth.rate))^2)
-  }
-  
-  
-  # Use the curve fit to find Topt and the estimated maximum growth rate (i.e. growth rate at Topt)
-  grfunc<-function(x){
-    -nbcurve(x,cfs[[1]],cfs[[2]],cfs[[3]],cfs[[4]])
-  }
-  optinfo<-optim(c(x=cfs[[1]]),grfunc)
-  opt<-optinfo$par[[1]]
-  maxgrowth<- -optinfo$value
-  
-  #stash results		
-  rsqr.list[i]<-rsqr
-  z.list[i]<-cfs[[1]]
-  w.list[i]<-cfs[[2]]
-  a.list[i]<-cfs[[3]]
-  b.list[i]<-cfs[[4]]
-  s.list[i]<-cfs[[5]]
-  topt.list[i]<-opt
-  maxgrowth.list[i]<-maxgrowth
-  n.list[i]<-length(dat$temperature)
-}
-
-
-fits<-data.frame(curve.id.list, topt.list,maxgrowth.list,z.list,w.list,a.list,b.list,rsqr.list,s.list,n.list)
-
-
 fits_vals <- fits_real_constant %>% 
   summarise_each(funs(mean, sd), z, w, a, b)
 
@@ -220,13 +74,14 @@ df <-  expand.grid(a = avals, b = bvals) %>%
   
 
 (fit_d <- nlsLM(cell_density ~ 800 * (1+(a*exp(b*temp)*(1-((temp-z)/(w/2))^2)))^(days),
-               data= cd,  
+               data= cells_days,  
                start=list(z=14.4,w=35,a=-0.2, b=-0.2),
                lower = c(0, 0, -0.2, -0.2),
                upper = c(30, 40, 1.2, 0.3),
                control = nls.control(maxiter=1024, minFactor=1/204800000)))
 
-
+tidy(fit_d)
+glance(fit_d)
 cd <- cells_days %>% 
   mutate(time_point = trunc(time_since_innoc_hours)) %>% 
   group_by(temp, time_point) %>% 
@@ -264,7 +119,7 @@ df_split <- df %>%
 
 output <- df_split %>%
   map_df(fit_growth, .id = "run") %>% 
-  filter(df.residual > 5, isConv == "TRUE") %>% 
+  filter(df.residual > 5) %>% 
   top_n(n = -1, wt = AIC)
 
 return(output)
@@ -273,13 +128,14 @@ return(output)
 
 samples <- rep(1, 1000)
 
-bootstrap_time_series_fits <- samples %>% 
+bootstrap_time_series_fitsb <- samples %>% 
   map_df(resample, .id = "replicate")
 
 write_csv(bootstrap_time_series_fits, "Tetraselmis_experiment/data-processed/bootstrap_time_series_fits.csv")
 
-sums <- bootstrap_time_series_fits %>% 
-  summarise_each(funs(mean, sd), z, w, a, b)
+sums <- bootstrap_time_series_fitsb %>% 
+  filter(a != 1.2) %>% 
+  summarise_each(funs(mean), z, w, a, b)
 
 bind_rows(sums, fits_vals) %>% View
 
@@ -306,8 +162,15 @@ tpc2<-function(x){
   res
 }
 
+k <- sums$a[[1]]/2
+
+tpc3<-function(x){
+  res<-(sums$a[[1]]*exp(sums$b[[1]]*x)*(1-((x-sums$z[[1]])/(sums$w[[1]]/2))^2))
+  res
+}
+
 p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-p + stat_function(fun = tpc2, color = "black", size = 1) +xlim(-4, 33) + 
+p + stat_function(fun = tpc3, color = "black", size = 1) +xlim(-10, 33) + 
   ylim(-2, 5) + geom_hline(yintercept = 0) +
   stat_function(fun = curve_constant_resamp, color = ic[3], size = 1.5, alpha = 0.7)
   
@@ -315,10 +178,13 @@ nbcurve<-function(temp,z,w,a,b){
   res<-a*exp(b*temp)*(1-((temp-z)/(w/2))^2)
   res
 }
+
 nbcurve1<-function(temp,z,w,a,b){
   res<-a*exp(b*temp)*(1-((temp-z)/(w/2))^2)
   res
 }
+
+tp <- results
 
 grfunc<-function(x){
   -nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]])
@@ -331,7 +197,7 @@ maxgrowth <- -optinfo$value
 
 library(rootSolve)
 uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(opt,150))
-uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(-3,opt))
+uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(-15,opt))
 
 
 tidy(fit_d) %>% View
---
+++ Tetraselmis_experiment/R/38_time_reseries_fitting.R
@@ -0,0 +1,64 @@
+
+
+avals<-seq(-0.2,1.2,0.02)
+bvals<-seq(-0.2,0.3,0.02)
+
+
+df <-  expand.grid(a = avals, b = bvals) %>% 
+  mutate(unique_id = rownames(.)) %>% 
+  mutate(sample_size = 1)
+
+
+cells_days <- cells_exp %>% 
+  mutate(days = time_since_innoc_hours/24) %>% 
+  mutate(days = ifelse(days < 0, 0, days))
+
+fit_growth <- function(df){
+  res <- try(nlsLM(cell_density ~ 800 * (1+(a*exp(b*temp)*(1-((temp-z)/(w/2))^2)))^(days),
+                   data= cells_days  
+                   start=list(z=14.4,w=35,a= df$a[[1]], b=df$b[[1]]),
+                   lower = c(0, 0, -0.2, -0.2),
+                   upper = c(30, 45, 1.2, 0.3),
+                   control = nls.control(maxiter=1024, minFactor=1/204800000)))
+  if(class(res)!="try-error"){
+    out1 <- tidy(res) %>% 
+      select(estimate, term) %>% 
+      spread(key = term, value = estimate)
+    out2 <- glance(res)
+  }
+  all <- bind_cols(out1, out2)
+  all
+}
+
+
+df_split <- df %>% 
+  split(.$unique_id)
+
+
+output <- df_split %>%
+  map_df(fit_growth, .id = "run") 
+
+
+results <- output %>% 
+  filter(df.residual > 5) %>%
+  top_n(n = -1, wt = AIC)
+
+
+write_csv(results, "Tetraselmis_experiment/data-processed/time_resampling_fits.csv")
+
+
+tp <- results
+
+grfunc<-function(x){
+  -nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]])
+}
+
+### find limits
+optinfo<-optim(c(x=tp$z[[1]]),grfunc)
+opt <-optinfo$par[[1]]
+maxgrowth <- -optinfo$value
+
+library(rootSolve)
+uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(opt,150))
+uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(-15,opt))
+
