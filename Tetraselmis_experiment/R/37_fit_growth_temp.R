a8366df238e28eb8d122533c091a04c56c9a49b6
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-02-11 18:25:56 -0800

    ok now have methods to estimate the temperature dependence of r all at once

--- Tetraselmis_experiment/R/37_fit_growth_temp.R
+++ Tetraselmis_experiment/R/37_fit_growth_temp.R
@@ -205,11 +205,89 @@ fit_c <- nlsLM(cell_density ~ 800 * (1+(a*exp(b*temp)*(1-((temp-z)/(w/2))^2)))^(
     data= cells_days,  start=list(z=16,w=36,a=0.2,b=0.1),
     control = nls.control(maxiter=1000, minFactor=1/204800000))
 
+avals<-seq(-0.2,1.2,0.02)
+bvals<-seq(-0.2,0.3,0.02)
+
+
+df <-  expand.grid(a = avals, b = bvals) %>% 
+  mutate(unique_id = rownames(.))
+
 fit_d <- nlsLM(cell_density ~ 800 * (1+(a*exp(b*temp)*(1-((temp-z)/(w/2))^2)))^(days),
-               data= cells_days,  start=list(z=14.19,w=36,a=0.2,b=0.1),
-               lower = c(15, 10, -0.2, -0.2),
-               upper = c(30, 70, 1.2, 0.3),
-               control = nls.control(maxiter=1000, minFactor=1/204800000))
+               data= cells_days,  
+               start=list(z=14.4,w=35,a=-0.2, b=-0.2),
+               lower = c(0, 0, -0.2, -0.2),
+               upper = c(30, 40, 1.2, 0.3),
+               control = nls.control(maxiter=1024, minFactor=1/204800000))
+
+
+fit_growth <- function(df){
+  res <- try(nlsLM(cell_density ~ 800 * (1+(a*exp(b*temp)*(1-((temp-z)/(w/2))^2)))^(days),
+      data= cells_days,  
+      start=list(z=14.4,w=35,a= df$a[[1]], b=df$b[[1]]),
+      lower = c(0, 0, -0.2, -0.2),
+      upper = c(30, 40, 1.2, 0.3),
+      control = nls.control(maxiter=1024, minFactor=1/204800000)))
+  if(class(res)!="try-error"){
+    out1 <- tidy(res) %>% 
+      select(estimate, term) %>% 
+      spread(key = term, value = estimate)
+    out2 <- glance(res)
+    
+  }
+  all <- bind_cols(out1, out2)
+  all
+}
+
+
+df_split <- df %>% 
+  split(.$unique_id)
+
+
+output <- df_split %>%
+  map_df(fit_growth, .id = "run") 
+  
+tp <- output %>% 
+  top_n(n = -1, wt = AIC) 
+
+cfd <- (coef(fit_d))
+
+tpc1<-function(x){
+  res<-(cfd[["a"]]*exp(cfd[["b"]]*x)*(1-((x-cfd[["z"]])/(cfd[["w"]]/2))^2))
+  res
+}
+
+tpc2<-function(x){
+  res<-(tp$a[[1]]*exp(tp$b[[1]]*x)*(1-((x-tp$z[[1]])/(tp$w[[1]]/2))^2))
+  res
+}
+
+p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
+p + stat_function(fun = tpc2, color = "black", size = 1) +xlim(-4, 33) + 
+  ylim(-2, 5) + geom_hline(yintercept = 0) +
+  stat_function(fun = curve_constant_resamp, color = ic[3], size = 1.5, alpha = 0.7)
+  
+nbcurve<-function(temp,z,w,a,b){
+  res<-a*exp(b*temp)*(1-((temp-z)/(w/2))^2)
+  res
+}
+nbcurve1<-function(temp,z,w,a,b){
+  res<-a*exp(b*temp)*(1-((temp-z)/(w/2))^2)
+  res
+}
+
+grfunc<-function(x){
+  -nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]])
+}
+
+?optim
+optinfo<-optim(c(x=tp$z[[1]]),grfunc)
+opt <-optinfo$par[[1]]
+maxgrowth <- -optinfo$value
+
+library(rootSolve)
+uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(opt,150))
+uniroot.all(function(x) nbcurve(x, z = tp$z[[1]],w = tp$w[[1]],a = tp$a[[1]],b = tp$b[[1]]),c(-3,opt))
+
 
 tidy(fit_d) %>% View
 tidy(fit_d) %>% View
@@ -245,29 +323,44 @@ growth_fun_29 <- function(x, temp = 29){
   res
 }
 
+growth_fun_32 <- function(x, temp = 32){
+  res <- 800 *  (1+(cfd[["a"]]*exp(cfd[["b"]]*temp)*(1-((temp-cfd[["z"]])/(cfd[["w"]]/2))^2)))^(x)
+  res
+}
+growth_fun_0 <- function(x, temp = 0){
+  res <- 800 *  (1+(cfd[["a"]]*exp(cfd[["b"]]*temp)*(1-((temp-cfd[["z"]])/(cfd[["w"]]/2))^2)))^(x)
+  res
+}
+growth_fun_20 <- function(x, temp = 20){
+  res <- 800 *  (1+(cfd[["a"]]*exp(cfd[["b"]]*temp)*(1-((temp-cfd[["z"]])/(cfd[["w"]]/2))^2)))^(x)
+  res
+}
+
+
+library(viridis)
+library(colormap)
+library(cowplot)
+ic <- colormap(colormap = colormaps$viridis, nshades = 9, format = "hex",
+               alpha = 1, reverse = FALSE)
+
 cells_days %>% 
-  ggplot(aes(x = days, y = cell_density, color = factor(temp))) + geom_point() +
-  stat_function(fun = growth_fun_27, color = "black") +ylim(0, 30000) +
-  stat_function(fun = growth_fun_5, color = "black") +
-  stat_function(fun = growth_fun_10, color = "black") +
-  stat_function(fun = growth_fun_24, color = "black") +
-  stat_function(fun = growth_fun_16, color = "black") +
-  stat_function(fun = growth_fun_29, color = "black") +
-  theme_bw() 
+  ggplot(aes(x = days, y = cell_density, color = factor(temp))) + geom_point(size = 2) +
+  geom_point(size = 2, shape= 1, color = "black") +
+  stat_function(fun = growth_fun_27, color = ic[7], size = 1) +ylim(0, 30000) +
+  stat_function(fun = growth_fun_5, color = ic[2], size = 1) +
+  stat_function(fun = growth_fun_10, color = ic[3], size = 1) +
+  stat_function(fun = growth_fun_24, color = ic[6], size = 1) +
+  stat_function(fun = growth_fun_16, color = ic[4], size = 1) +
+  stat_function(fun = growth_fun_29, color = ic[8], size = 1) +
+  stat_function(fun = growth_fun_32, color = ic[9], size = 1) +
+  stat_function(fun = growth_fun_0, color = ic[1], size = 1) +
+  stat_function(fun = growth_fun_20, color = ic[5], size = 1) +
+   scale_color_viridis(discrete = TRUE) +
+  ylab("Cell density") + xlab("Days")
 
 
 
 cf <- (coef(fit_c))
-cfd <- (coef(fit_d))
-
-tpc1<-function(x){
-  res<-(cfd[["a"]]*exp(cfd[["b"]]*x)*(1-((x-cfd[["z"]])/(cfd[["w"]]/2))^2))
-  res
-}
-
-p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-p + stat_function(fun = tpc1, color = "black", size = 1) +xlim(-2, 33) + 
-  ylim(-2, 5)
 
 
 expected<-nbcurve(dat$temperature,cfd[[1]],cfd[[2]],cfd[[3]],cfd[[4]])
